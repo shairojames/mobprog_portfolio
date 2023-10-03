6a8cd9aeeb95c53eb9a8898a2590dc02c37c7109
shairojames <146497320+shairojames@users.noreply.github.com>
2023-10-04 00:22:09 +0800

    Add files via upload

---
+++ Pahunang, Shairo Portfolio/style.js
@@ -0,0 +1,24 @@
+var typed = new Typed(".educ-text", {
+    strings: ["Educational Background"],
+    typeSpeed: 100,
+    backspeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
+var typed = new Typed(".people-text", {
+    strings: ["People Arround Me"],
+    typeSpeed: 100,
+    backspeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
+
+var typed = new Typed(".info-text", {
+    strings: ["Contact Info"],
+    typeSpeed: 100,
+    backspeed: 100,
+    backDelay: 1000,
+    loop: true
+})
